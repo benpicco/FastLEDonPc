6f8ae294b2d261117e0f39adaf1a08b2f3d6887e
Benjamin Valentin <benpicco@googlemail.com>
2018-12-11 00:51:30 +0100

    move hello world example to subdirectory

--- example/hello_world/hello.ino
+++ example/hello_world/hello.ino
@@ -1,42 +1,20 @@
-#define FASTLED_ESP8266_DMA
-
 #include <Adafruit_GFX.h>
 #include <FastLED.h>
 #include <FastLED_NeoMatrix.h>
 
 #define ARRAYSIZE(a) (sizeof(a) / sizeof(*a))
 
-// fast led constants
-#define DATA_PIN	3	// labeled as RX on nodeMCU
-#define COLOR_ORDER	GRB	// if colors are mismatched; change this
-
 #define M_WIDTH 	75	// the width of the LED matrix
 #define M_HEIGHT	8	// the height of the LED matrix
 #define NUM_LEDS	(M_WIDTH*M_HEIGHT)
 
-#ifndef WIFI_SSID
-#define WIFI_SSID	"changeme" // WiFi to connect to
-#endif
-#ifndef WIFI_PASS
-#define WIFI_PASS	"changeme" // WiFi password
-#endif
-
-// change WS2812B to match your type of LED, if different
-// list of supported types is here:
-// https://github.com/FastLED/FastLED/wiki/Overview
-#define LED_TYPE    SDL
-
 static CRGB leds[NUM_LEDS];
 static FastLED_NeoMatrix matrix = FastLED_NeoMatrix(leds, M_WIDTH, M_HEIGHT, NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS);
-// static FastLED_NeoMatrix matrix = FastLED_NeoMatrix(leds, M_WIDTH, M_HEIGHT, NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG);
 
 void setup()
 {
 	FastLED.addLeds<SDL, M_WIDTH, M_HEIGHT>(leds, NUM_LEDS);
-//	FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 	FastLED.setCorrection(TypicalLEDStrip);
-//	FastLED.setBrightness(5);	// low brightness so we can test the strip just using USB
-	FastLED.clear(true);
 
 	matrix.begin();
 
